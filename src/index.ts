9c6690d6d61035896a2fb0350e106c3902ec8562
Joseph Ero <erojoseph94@gmail.com>
2023-02-04 11:51:08 +0100

    chore(project) initialize project and setup the folder structure

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors"
+import helmet from "helmet";
+
+const app = express()
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
