15687da2fc0a82a6589580c77210c265393d0475
Joseph Ero <erojoseph94@gmail.com>
2023-02-04 12:14:20 +0100

    chore(setup) added dev script

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import helmet from "helmet";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
+const PORT = process.env.PORT;
+
+const app = express();
 
 app.use(cors());
 app.use(helmet());
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+app.get("/", (_req, res) => {
+  res
+    .status(200)
+    .send("<h1>Hello World</h1><p>Welcome to SavingBuddy API.</p>");
+});
+
+app.use((_req, res) =>
+  res
+    .status(404)
+    .json({ status: "Not Found", message: "This route does not exist" })
+);
+
+app.listen(PORT || 3000, () => {
+  console.log(`Server started on PORT ${PORT} 🚀 `);
+});
