7c0d85e60a0c414b9cd287c59bc19a576ff2c552
Joseph Ero <erojoseph94@gmail.com>
2023-02-05 17:32:24 +0100

    feature(verify-user) endpoint to verify the user email address using JWT token

--- src/api/v1/routes/userRoute.ts
+++ src/api/v1/routes/userRoute.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-  import validate from "../middleware/validation";
-  import { register, signIn } from "../validationSchema/user";
-  import * as Controller from "../controllers/userController"
+import validate from "../middleware/validation";
+import { register, signIn } from "../validationSchema/user";
+import * as Controller from "../controllers/userController";
 
-const router = Router()
+const router = Router();
 
-router.post("/signin", validate(signIn), Controller.signIn)
-router.post("/register", validate(register), Controller.register)
+router.post("/signin", validate(signIn), Controller.signIn);
+router.post("/register", validate(register), Controller.register);
+router.get("/verify/:token", Controller.verify);
 
 export default router;
